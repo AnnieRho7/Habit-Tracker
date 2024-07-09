dfbb1178f99754e10f5cca64980c2b133b7d9dd0
AnnieRho7 <annierhodes17@gmail.com>
2024-07-09 22:15:55 +0000

    added DOM event listener

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,5 @@
-console.log("Hello world!");
+document.addEventListener('DOMContentLoaded', function() {
+    // Code here will execute when the DOM is fully loaded and parsed
+});
 
+console.log("123")
